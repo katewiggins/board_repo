5b370027c97c00716d08097e20a0b19c2c0a9c36
katewiggins <144843460+katewiggins@users.noreply.github.com>
2023-12-11 19:55:44 -0500

    done

--- javascript/script.js
+++ javascript/script.js
@@ -1,81 +1,120 @@
 const audioStates = {};
 
-// Function to play or stop audio based on its current state
-function toggleAudio(audioSrc, audioId) {
-    if (audioStates[audioId] && audioStates[audioId].isPlaying) {
-        stopAudio(audioId);
-    } else {
-        playAudio(audioSrc, audioId);
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll('.button');
+    const sliders = document.querySelectorAll('.audio-container input[type="range"]');
+    const crossfaderSlider = document.getElementById('crossfader');
+    const crossfadeButton = document.querySelector('.faderButton');
+    const knob = document.getElementById('knob');
+    const indicator = document.getElementById('indicator');
 
-// Function to play audio
-function playAudio(audioSrc, audioId) {
-    // Check if the audio is not playing
-    if (!audioStates[audioId] || !audioStates[audioId].isPlaying) {
-        // Create an audio element if not already present
-        if (!audioStates[audioId]) {
-            audioStates[audioId] = new Audio(audioSrc);
-        }
+    let isDragging = false;
 
-        // Play the audio
-        audioStates[audioId].play();
-        audioStates[audioId].isPlaying = true;
-    }
-}
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            let audioSrc;
 
-// Function to stop audio
-function stopAudio(audioId) {
-    // Check if the audio is playing
-    if (audioStates[audioId] && audioStates[audioId].isPlaying) {
-        // Pause and reset the audio
-        audioStates[audioId].pause();
-        audioStates[audioId].currentTime = 0;
-        audioStates[audioId].isPlaying = false;
-    }
-}
-
-// Function to stop all playing audio
-function stopAllAudio() {
-    for (const key in audioStates) {
-        if (audioStates.hasOwnProperty(key) && audioStates[key].isPlaying) {
-            stopAudio(key);
-        }
-    }
-}
-
-// Event listener for the slider input
-volume.addEventListener("input", (e) => {
-    const sliderValue = e.currentTarget.value / 100;
-
-    // Set volumes based on slider position for each audio element
-    audioStates['audio1'].volume = 1 - sliderValue;  // Switched the volumes
-    audioStates['audio2'].volume = sliderValue;
-});
-
-// Play button click event
-play.addEventListener("click", () => {
-    let isPlaying = false;
-
-    for (const key in audioStates) {
-        if (audioStates.hasOwnProperty(key)) {
-            const audio = audioStates[key];
-            if (audio.isPaused) {
-                audio.play();
-                audio.isPaused = false;
-                isPlaying = true;
-            } else {
-                audio.pause();
-                audio.isPaused = true;
+            if (index === 0) {
+                audioSrc = 'audio/dev_bassdownlow.mp3';
+            } else if (index === 1) {
+                audioSrc = 'audio/juniorjack_myfeeling.mp3';
             }
+
+            toggleAudio(audioSrc, `audio${index + 1}`, `slider${index + 1}`);
+        });
+    });
+
+    const button5 = document.getElementById('button5');
+    button5.addEventListener("click", function () {
+        toggleAudio('audio/bass_high.mp3', 'audio5');
+    });
+
+    const button6 = document.getElementById('button6');
+    button6.addEventListener("click", function () {
+        toggleAudio('audio/jack_low.mp3', 'audio6');
+    });
+
+    sliders.forEach((slider, index) => {
+        slider.addEventListener("input", (event) => {
+            adjustVolume(`audio${index + 1}`, event.target.value);
+        });
+    });
+
+    crossfadeButton.addEventListener("click", startBothSongs);
+    crossfaderSlider.addEventListener("input", updateCrossfadeVolumes);
+
+    knob.addEventListener('mousedown', function (e) {
+        isDragging = true;
+        document.addEventListener('mousemove', handleDrag);
+        document.addEventListener('mouseup', function () {
+            isDragging = false;
+            document.removeEventListener('mousemove', handleDrag);
+        });
+    });
+
+    function startBothSongs() {
+        const bassAudio = getOrCreateAudio('audio/dev_bassdownlow.mp3', 'audio1', 'slider1');
+        const jackAudio = getOrCreateAudio('audio/juniorjack_myfeeling.mp3', 'audio2', 'slider2');
+
+        bassAudio.play();
+        jackAudio.play();
+    }
+
+    function updateCrossfadeVolumes() {
+        const bassAudio = audioStates['audio1'].audio;
+        const jackAudio = audioStates['audio2'].audio;
+
+        const crossfadeValue = crossfaderSlider.value / 100;
+        const bassVolume = 1 - crossfadeValue;
+        const jackVolume = crossfadeValue;
+
+        bassAudio.volume = bassVolume;
+        jackAudio.volume = jackVolume;
+    }
+
+    function toggleAudio(audioSrc, audioId, sliderId) {
+        const audio = getOrCreateAudio(audioSrc, audioId, sliderId);
+
+        if (audio.paused || audio.ended) {
+            playAudio(audio);
+        } else {
+            stopAudio(audio);
         }
     }
 
-    if (isPlaying) {
+    function getOrCreateAudio(audioSrc, audioId, sliderId) {
+        if (!audioStates[audioId]) {
+            const audio = new Audio(audioSrc);
+            audio.volume = 1.0;
+            audioStates[audioId] = { audio, sliderId };
+        }
+        return audioStates[audioId].audio;
+    }
+
+    function playAudio(audio) {
+        stopAllAudio();
+        audio.play();
+    }
+
+    function stopAudio(audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+
+    function stopAllAudio() {
         for (const key in audioStates) {
             if (audioStates.hasOwnProperty(key)) {
-                stopAudio(key);
+                stopAudio(audioStates[key].audio);
             }
         }
     }
+
+    function adjustVolume(audioId, volume) {
+        const audio = audioStates[audioId].audio;
+        audio.volume = volume / 100;
+    }
+
+    function handleDrag(e) {
+    }
+
 });
